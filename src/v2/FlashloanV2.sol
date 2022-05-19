1d08ed1dded6f5f455d33055a786658f15ce7652
byhow <by.how@hotmail.com>
2022-05-18 22:27:45 -0700

    chore: add more interface for receiver

--- src/v2/FlashloanV2.sol
+++ src/v2/FlashloanV2.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "./aave/FlashLoanReceiverBaseV2.sol";
+import "aave/interfaces/v2/ILendingPoolAddressesProviderV2.sol";
+import "aave/interfaces/v2/ILendingPoolV2.sol";
+
+contract FlashloadV2 is IFlashLoanReceiverV2, Withdrawable {
+  
+}
